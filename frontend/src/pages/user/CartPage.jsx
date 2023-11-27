9d5fa561a300f360f4926d0e107e7a3e2d186d59
nabeelktr <nabee446@gmail.com>
2023-11-27 23:29:57 +0530

    structure modified

--- frontend/src/pages/user/CartPage.jsx
+++ frontend/src/pages/user/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/user/Navbar'
-import Cart from '../../components/user/cart/cart'
+import Cart from '../../components/user/cart/Cart'
 import { Route, Routes } from 'react-router-dom'
 import AddAddress from '../../components/user/cart/addAddress/AddAddress'
 import Payment from '../../components/user/cart/Payment/Payment'
